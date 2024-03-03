7dd7725a3b15866eb67ac8c38e7b4d08ea895d13
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2024-03-03 23:21:17 +0900

    그리디 학습(1)

--- main.cpp
+++ main.cpp
@@ -1,33 +1,20 @@
-#define _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
-#include <string>
+#include <algorithm>
 using namespace std;
-class Car {
-    private:
-        int num;
-        double gas;
-        char* pName;
-    public:
-        Car(char* pN, int n, double g);
-        ~Car();
-        void show();
-};
-Car::Car(char* pN, int n, double g) {
-    pName = new char[strlen(pN) + 1];
-    strcpy(pName, pN);
-    num = n;
-    gas = g;
-    cout << pName << "(번호 : " << num << ", 연료량 : " << gas << ") 생산 완료"  << '\n';
-}
-Car::~Car() {
-    cout << pName << " 소멸 \n";
-    delete[] pName;
-}
-void Car::show() {
-    cout << "이름 : " << pName << ", 번호 : " << num << ", 연료량 : " << gas << '\n';
-}
-int main() {
-    Car benz("벤츠", 1234, 25.5);
-    benz.show();
+int main()
+{
+    int n,m,k,sum=0;
+    cin>>n>>m>>k;
+    int* arr = new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr, arr+n);
+    int count = (m/(k+1))*k;
+    count += m%(k+1);
+    sum += arr[n-1]*count;
+    sum += arr[n-2]*(m-count);
+    cout << sum << '\n';
     return 0;
 }
