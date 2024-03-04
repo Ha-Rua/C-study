9b06b5d8d4b859934dc9580b1cf31000e312e2c4
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2024-03-04 14:17:19 +0900

    그리디 학습(3)

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 int main()
 {
     int n,m;
     cin>>n>>m;
-    int** array = new int*[n];
+    int result=0,cur;
     for(int i=0;i<n;i++)
-        array[i] = new int[m];
-    for(int i=0;i<n;i++){
-        for (int j=0;j<m;j++)
-            cin>>array[i][j];
-    }
-    int target=0;
-    for (int i=0;i<n;i++)
     {
-        sort(array[i],array[i]+m);
-        if(array[i][0]>target)
-            target=array[i][0];
+        int min = 10001;
+        for(int j=0;j<m;j++){
+            cin>>cur;
+            if(cur<min)
+                min=cur;
+        }
+        if (min>result)
+            result=min;
     }
-    cout << target;
+    cout<<result<<'\n';
     return 0;
 }
