bd6a70396c7c69604763748ee4ac3eeaeac1bb58
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2024-03-11 00:30:51 +0900

    대입 연산자와 복사 생성자

--- main.cpp
+++ main.cpp
@@ -1,20 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
+#include <string>
 using namespace std;
-int main()
-{
-    int n,k;
-    cin>>n>>k;
-    int result=0;
-    while(true){
-        int target=n/k*k;
-        result +=(n-target);
-        n=target;
-        if (n<k)
-            break;
-        result++;
-        n/=k;
+class Car {
+    private:
+        int num;
+        double gas;
+        char* pName;
+    public:
+        Car(char* pN, int n, double g);
+        Car(const Car& c);
+        Car& operator=(const Car& c);
+        ~Car();
+        void show();
+};
+Car::Car(char* pN, int n, double g) {
+    pName = new char[strlen(pN) + 1];
+    strcpy(pName, pN);
+    num = n;
+    gas = g;
+    cout << pName << "(번호 : " << num << ", 연료량 : " << gas << ") 생산 완료"  << '\n';
+}
+Car::Car(const Car& c) {
+    cout<< c.pName << "으로 초기화 \n";
+    pName = new char[strlen(c.pName) + strlen("의 복사본 1") + 1];
+    strcpy(pName, c.pName);
+    strcat(pName, "의 복사본 1");
+    num = c.num;
+    gas = c.gas;
+}
+Car& Car::operator=(const Car& c){
+    cout<< pName << "에 " << c.pName << "을 대입 \n";
+    if (this != &c) {
+        delete[] pName;
+        pName = new char[strlen(c.pName) + strlen("의 복사본 2") + 1];
+        strcpy(pName, c.pName);
+        strcat(pName, "의 복사본 2");
+        num = c.num;
+        gas = c.gas;
     }
-    result+=(n-1);
-    cout<<result<<'\n';
+}
+
+Car::~Car() {
+    cout << pName << " 소멸 \n";
+    delete[] pName;
+}
+void Car::show() {
+    cout << "이름 : " << pName << ", 번호 : " << num << ", 연료량 : " << gas << '\n';
+}
+int main() {
+    Car benz("benz", 1234, 25.5);
+    benz.show();
+    Car benz2 = benz;
+    benz2.show();
+    Car benz3("benz3",0,0);
+    benz3 = benz;
+    benz3.show();
     return 0;
 }
