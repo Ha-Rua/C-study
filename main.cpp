8ea7226ea3d04913ad053449aee84f9d76f5ca12
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2024-03-14 22:53:46 +0900

    파일 입출력 기능1

--- main.cpp
+++ main.cpp
@@ -1,101 +1,17 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
-class Point
-{
-    private:
-        int x,y;
-    public:
-        Point(int a=0,int b=0){x=a,y=b;}
-        void setX(int a){x=a;}
-        void setY(int b){y=b;}
-        void show(){cout<<"x : "<<x<<", y : "<<y<<'\n';}
-        Point operator++();
-        Point operator++(int d);
-        Point operator--();
-        Point operator--(int d);
-        friend Point operator+(Point p1,Point p2);
-        friend Point operator-(Point p1,Point p2);
-        friend Point operator+(int a,Point p);
-        friend Point operator-(int a,Point p);
-        friend Point operator+(Point p,int a);
-        friend Point operator-(Point p,int a);
-};
-Point Point::operator++()
-{
-    x++;
-    y++;
-    return *this;
-}
-Point Point::operator++(int d)
-{
-    Point p = *this;
-    x++;
-    y++;
-    return *this;
-}
-Point Point::operator--()
-{
-    x--;
-    y--;
-    return *this;
-}
-Point Point::operator--(int d)
-{
-    Point p = *this;
-    x--;
-    y--;
-    return *this;
-}
-Point operator+(Point p1,Point p2)
-{
-    return Point(p1.x+p2.x,p1.y+p2.y);
-}
-Point operator-(Point p1,Point p2)
-{
-    return Point(p1.x-p2.x,p1.y-p2.y);
-}
-Point operator+(int a,Point p)
-{
-    return Point(a+p.x,a+p.y);
-}
-Point operator-(int a,Point p)
-{
-    return Point(a-p.x,a-p.y);
-}
-Point operator+(Point p,int a)
-{
-    return Point(p.x+a,p.y+a);
-}
-Point operator-(Point p,int a)
-{
-    return Point(p.x-a,p.y-a);
-}
 int main()
 {
-    Point p1,p2;
-    p1.setX(10);
-    p1.setY(20);
-    p2.setX(30);
-    p2.setY(40);
-    p1.show();
-    p2.show();
-    p1++;
-    p1.show();
-    p1--;
-    p1.show();
-    ++p2;
-    p2.show();
-    --p2;
-    p2.show();
-    Point p3 = p1+p2;
-    p3.show();
-    p3 = p3+1;
-    p3.show();
-    p3 = p3-2;
-    p3.show();
-    p3 = 3+p3;
-    p3.show();
-    p3 = 3-p3;
-    p3.show();
+    ifstream fin("test0.txt");
+    if(!fin){
+        cout << "파일을 열 수 없음 \n";
+        return 1;
+    }
+    string str1, str2;
+    fin >> str1 >> str2;
+    cout << "파일이 기록된 문자열 : " << str1 << ", " << str2;
+    fin.close();
     return 0;
 }
