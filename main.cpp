2dbec3f8fb73d6c9b603e36cde7d8edf220c6696
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2024-03-15 17:08:30 +0900

    파일 읽어들이기

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
-#include <fstream>
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <iomanip>
 using namespace std;
 int main()
 {
-    ifstream fin("test0.txt");
+    ifstream fin("test2.txt");
     if(!fin){
         cout << "파일을 열 수 없음 \n";
         return 1;
     }
-    string str1, str2;
-    fin >> str1 >> str2;
-    cout << "파일이 기록된 문자열 : " << str1 << ", " << str2;
+    const int  num = 8;
+    int score[num];
+    for(int i=0; i<num; i++){
+        fin >> score[i];
+    }
+    int max = score[0];
+    int min = score[0];
+    for(int i=0; i<num; i++){
+        if(max < score[i])
+            max = score[i];
+        if(min > score[i])
+            min = score[i];
+        cout << i+1 << "번째 사람의 시험 점수 : " << score[i] << '\n';
+    }
+    cout << "최고 점수 : " << max << '\n';
+    cout << "최저 점수 : " << min << '\n';
     fin.close();
     return 0;
 }
