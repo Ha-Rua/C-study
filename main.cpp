65debab9a375e67e7555910525fe38d0e06fd3a0
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2024-03-24 23:17:17 +0900

    알고리즘 학습 4장 (2)

--- main.cpp
+++ main.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
-#include <utility>
 using namespace std;
+inline void turn_left(int& d) {d = (d + 3) % 4;}
 int main()
 {
-    char px[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-    pair<char, int> position;
-    cin >> position.first >> position.second;
-    int dx, dy;
-    for (int i = 0; i < 8; i++) {
-        if (px[i] == position.first)
-            dx = i + 1;
+    int map[50][50];
+    int check[50][50];
+    int n, m;
+    cin >> n >> m;
+    int a,b,d;
+    cin >> a >> b >> d;
+    check[a][b] = 1;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++)
+            cin >> map[i][j];
     }
-    dy = position.second;
-    int res = 0;
-    int movex[8] = {1,1,2,2,-1,-1,-2,-2};
-    int movey[8] = {2,-2,1,-1,2,-2,1,-1};
-    for (int i = 0; i < 8; i++) {
-        if(dx + movex[i]<1 || dx + movex[i]>8 || dy + movey[i]<1 || dy + movey[i]>8)
+    int dx[4] = {-1,0,1,0};
+    int dy[4] = {0,1,0,-1};
+    int res = 1;
+    while(true)
+    {
+        int turn_cnt=0;
+        turn_left(d);
+        int nx = a + dx[d];
+        int ny = b + dy[d];
+        if(map[nx][ny] == 0 && check[nx][ny] == 0)
+        {
+            check[nx][ny] = 1;
+            a = nx;
+            b = ny;
+            res++;
+            turn_cnt = 0;
             continue;
-        res++;
+        }
+        else turn_cnt++;
+        if(turn_cnt == 4)
+        {
+            nx = a - dx[d];
+            ny = b - dy[d];
+            if(map[nx][ny] == 0)
+            {
+                a = nx;
+                b = ny;
+            }
+            else break;
+            turn_cnt = 0;
+        }
     }
     cout << res;
     return 0;
